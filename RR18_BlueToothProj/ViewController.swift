4aea7e6e9a0708f1f493da5b49c2d41034657cf2
hweelin-yeo <hy384@cornell.edu>
2018-10-06 16:54:08 -0400

    Initial Commit

---
+++ RR18_BlueToothProj/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RR18_BlueToothProj
+//
+//  Created by Yeo Hwee Lin on 10/6/18.
+//  Copyright © 2018 Yeo Hwee Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
